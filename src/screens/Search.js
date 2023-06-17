b9cac65e4871138b9347f3cbb77c54c294ab7e5b
fmoche <fmoche@udesa.edu.ar>
2023-06-16 23:26:36 -0300

    MY SOLDIERS RAGEgit add .

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TextInput, FlatList } from 'react-native'
+import { db } from '../firebase/config'
 
 export default class Search extends Component {
-  render() {
+    constructor(props){
+      super(props)
+      this.state={
+        searched:'',
+        users:[],
+        usersBackup:[]
+      }
+    }
+
+    componentDidMount(){
+      db.collection('users').onSnapshot(
+        docs => {
+          let usersArray = []
+          docs.forEach(doc => {
+            usersArray.push({
+              id: doc.id,
+              data: doc.data()
+            })
+          })
+          this.setState({
+            users:usersArray,
+            usersBackup:usersArray
+          })
+        }
+      )
+    }
+
+    filter(willFilter){
+      let filteredUsers = this.state.usersBackup
+        .filter(user =>{user.data.username.toLowerCase()
+        .includes(willFilter.toLowerCase)})
+        this.setState({users:filteredUsers})
+    }
+
+    render() {
     return (
       <View>
         <Text> textInComponent </Text>
+        <TextInput
+          placeholder='wanna search?'
+          onChangeText={(text)=> this.filter(text)}
+        
+        />
+        <FlatList 
+        data={this.state.users}
+        keyExtractor={item => item.id.toString()}
+        renderItem = {({item})=><Text>{item.data.username}</Text>}
+        />
       </View>
     )
-  }
+    }
 }
