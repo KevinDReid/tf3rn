62dcdf4efebd5cebad621071a170d133111b5f4a
fmoche <fmoche@udesa.edu.ar>
2023-06-17 00:09:25 -0300

    facherin

--- src/screens/Me.js
+++ src/screens/Me.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, FlatList, StyleSheet, ActivityIndicator } from 'react-native'
+import { Text, View, FlatList, StyleSheet, ActivityIndicator, Image } from 'react-native'
 import { db, auth } from '../firebase/config';
 import Logout from '../components/me/Logout';
 import Card from "../components/card/Card";
@@ -56,42 +56,66 @@ export default class Me extends Component {
 
   render() {
     return (
-<View style={{flex:1}}>
-  <Text> {this.state.user.length > 0 ? this.state.user[0].data.bio : ''} </Text>
-  <Logout navigation={this.props.navigation}/>
-        <section style={styles.cardSection}>
-        {
-  this.state.loading ?
-  <ActivityIndicator size='large' color='blue'/> : (
-    <FlatList 
-      data={this.state.posts}
-      keyExtractor={(item,index) => index.toString()}
-      renderItem={({ item }) => (
-        <Card
-          id={item.id}
-          myLike={item.data.whoLiked.some(email => email == auth.currentUser.email)}
-          navigation={this.props.navigation}
-          email={item.data.owner}
-          profImg={item.data.profImg}
-          img={item.data.photo}
-          desc={item.data.descript}
-          likes={item.data.likes}
-          commentNumber={this.state.comments}
-        />
-      )}
-    />
-  ) 
-}
-        </section>
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  cardSection: {
-    overflow:'auto',
-    display: "flex",
-    justifyContent: "center",
-  },
-});
+      <View><>
+      {
+          this.state.loading ? <ActivityIndicator size='large' color='blue'/> :<>
+          <section style={styles.topside}>
 
+          <div style={styles.profilePicCont}>
+
+          <Image source={{uri:this.state.user[0].data.pfp}} style={styles.profilePic} resizeMode='contain'/>
+          </div>
+      <div style={styles.info}>
+
+          <Text style={{fontSize:25, fontWeight:'bold',}}>{this.state.user[0].data.username}</Text>
+          <Text> {this.state.user.length > 0 ? this.state.user[0].data.bio : ''} </Text>
+      </div>
+      </section>
+          <FlatList data={this.state.posts} 
+          keyExtractor={(item,index) => index.toString()}
+          renderItem={({ item }) => (
+              <Card
+              id={item.id}
+              myLike={item.data.whoLiked.some(email => email == auth.currentUser.email)}
+              navigation={this.props.navigation}
+              email={item.data.owner}
+                  profImg={item.data.pfp}
+                  img={item.data.photo}
+                  desc={item.data.descript}
+                  likes={item.data.likes}
+                  />
+                  )}
+                  />
+                  
+                  </>
+} 
+</>
+</View>
+                  )}
+              }
+let styles = StyleSheet.create({
+profilePic:{
+overflow:'visible',
+height: "100%",
+width: "auto",
+
+},
+profilePicCont:{
+width:100,
+height:100,
+borderRadius:'50%',
+display: "inline-block",
+overflow: "hidden",
+},
+topside:{
+display:'flex',
+justifyContent:'space-around',
+marginTop:10,
+marginBottom:10
+},
+info:{
+display:'flex',
+flexDirection:'column',
+justifyContent:'center'
+}
+})
